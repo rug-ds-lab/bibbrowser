656fb3e4a08442ad0fd6648b3428f45e5456a1c0
Heerko Groefsema <Heerkog@users.noreply.github.com>
2021-02-04 14:26:17 +0100

    Updated sorting method
    Previous version sorted by the order of the bib file, i.e., manual. Now ordering is automatically by year, then type, month, and finally author.

--- bibbrowser.php
+++ bibbrowser.php
@@ -2,7 +2,6 @@
 
 $_GET['library']=1;
 @define('BIBTEXBROWSER_BIBTEX_LINKS',false);
-@define('ORDER_FUNCTION','compare_bib_entry_by_bibtex_order');
 require('bibtexbrowser.php');
 
 $db = new BibDataBase();
@@ -25,7 +24,7 @@ function BibBrowseByYear($m) {
 	
 	$query = array('year'=>$m[1]);
 	$entries = $db->multisearch($query);
-	uasort($entries, 'compare_bib_entries');
+	uasort($entries, 'academicOrder');
 	
 	return printEntries($entries);
 }
@@ -35,7 +34,7 @@ function BibBrowseByYearRel($m) {
 	
 	$query = array('year'=>date("Y") + $m[1]);
 	$entries = $db->multisearch($query);
-	uasort($entries, 'compare_bib_entries');
+	uasort($entries, 'academicOrder');
 	
 	return printEntries($entries);
 }
@@ -45,7 +44,7 @@ function BibBrowseByLast($m) {
 	
 	$query = array('author'=>$m[1]);
 	$entries = $db->multisearch($query);
-	uasort($entries, 'compare_bib_entries');
+	uasort($entries, 'academicOrder');
 	
 	$entries = array_slice($entries, 0, $m[2]);
 	
@@ -58,7 +57,7 @@ function BibBrowseByAuth($m) {
 	$query = array('author'=>$m[1],
 				   'type'=>(isset($m[2]) ? $m[2] : '.*'));
 	$entries = $db->multisearch($query);
-	uasort($entries, 'compare_bib_entries');
+	uasort($entries, 'academicOrder');
 	
 	return printEntries($entries);
 }
@@ -68,7 +67,7 @@ function BibBrowseByType($m) {
 	
 	$query = array('type'=>$m[1]);
 	$entries = $db->multisearch($query);
-	uasort($entries, 'compare_bib_entries');
+	uasort($entries, 'academicOrder');
 	
 	return printEntries($entries);
 }
@@ -82,6 +81,62 @@ function printEntries($entries) {
 	$output .= '</ol>';
 	
 	return Keep($output);
+}
+
+function academicOrder($a, $b) {
+	$r = (($a->getYear() < $b->getYear()) ? 1 : (($a->getYear() > $b->getYear()) ? -1 : 0));
+	if($r == 0) $r = ((getTypeWeight($a) < getTypeWeight($b)) ? -1 : ((getTypeWeight($a) > getTypeWeight($b)) ? 1 :0));
+	if($r == 0) $r = strcmp($a->getField("month"), $b->getField("month"));
+	if($r == 0) $r = strcmp($a->getFormattedAuthorsString(), $b->getFormattedAuthorsString());
+	
+	return $r;
+}
+
+function getTypeWeight($e) {
+	$r = 6;
+	
+	switch ($e->getType()) {
+		case "book":
+			$r = 1;
+			break;
+		case "article":
+			$r = 3;
+			break;
+		case "booklet":
+			$r = 1;
+			break;
+		case "inbook":
+			$r = 1;
+			break;
+		case "incollection":
+			$r = 4;
+			break;
+		case "inproceedings":
+			$r = 4;
+			break;
+		case "manual":
+			$r = 5;
+			break;
+		case "mastersthesis":
+			$r = 5;
+			break;
+		case "phdthesis":
+			$r = 1;
+			break;
+		case "proceedings":
+			$r = 2;
+			break;
+		case "techreport":
+			$r = 5;
+			break;
+		case "unpublished":
+			$r = 6;
+			break;
+	    default:
+			$r = 6;
+	}
+	
+	return $r;
 }
 
 ?>
